e6a80ec77ea0080c3199ee1033dd95b2ac2089fb
Jeswin Madona <jeswin.madona.26@gmail.com>
2026-08-17 21:37:38 +0530

    Delete frontend/Quiz React directory

--- frontend/Quiz React/src/services/api.js
+++
@@ -1,8 +0,0 @@
-
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "http://localhost:5678"
-});
-
-export default api;
--- frontend/Quiz React/src/services/quizService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const getQuizQuestions = async (quizId) => {
-    const response = await api.get(`/webhook-test/get-quiz?id=${quizId}`);
-    return response.data;
-
-    
-
-};
